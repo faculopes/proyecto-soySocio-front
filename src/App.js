4858e37344e7ae85278a37e264bbd02751cca1cf
TomasNieto19 <tnieto852@gmail.com>
2024-04-06 19:39:45 -0300

    init react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>hola</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>todo bien</h2>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
